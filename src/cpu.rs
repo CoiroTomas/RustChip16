40169890b907eb6385c30d85867d5fa1312c92fd
CoiroTomas <tomcoiro@hotmail.com>
2014-08-18 21:50:26 -0300

    Decided on a structure for opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,68 @@
 use std::io::File;
 
+//branching condition functions
+fn zero(flag_byte: i8) -> bool {
+	(flag_byte & Zero as i8) == Zero as i8
+}
+
+fn non_zero(flag_byte: i8) -> bool {
+	!zero(flag_byte)
+}
+
+fn negative(flag_byte: i8) -> bool {
+	(flag_byte & Negative as i8) == Negative as i8
+}
+
+fn non_negative(flag_byte: i8) -> bool {
+	!negative(flag_byte)
+}
+
+fn positive(flag_byte: i8) -> bool {
+	non_zero(flag_byte) && non_negative(flag_byte)
+}
+
+fn overflow(flag_byte: i8) -> bool {
+	(flag_byte & Overflow as i8) == Overflow as i8
+}
+
+fn non_overflow(flag_byte: i8) -> bool {
+	!overflow(flag_byte)
+}
+
+fn above(flag_byte: i8) -> bool {
+	above_equal(flag_byte) && non_zero(flag_byte)
+}
+
+fn above_equal(flag_byte: i8) -> bool {
+	!below(flag_byte) 
+}
+
+fn below(flag_byte: i8) -> bool {
+	(flag_byte & Carry as i8) == Carry as i8
+}
+
+fn below_equal(flag_byte: i8) -> bool {
+	below(flag_byte) && zero(flag_byte)
+}
+
+fn signed_greater_than(flag_byte: i8) -> bool {
+	signed_greater_than_equal(flag_byte) && non_zero(flag_byte)
+}
+
+fn signed_greater_than_equal(flag_byte: i8) -> bool {
+	((flag_byte & Overflow as i8) == Overflow as i8)
+	== ((flag_byte & Negative as i8) == Negative as i8)
+}
+
+fn signed_less_than(flag_byte: i8) -> bool {
+	!signed_greater_than_equal(flag_byte)
+}
+
+fn signed_less_than_equal(flag_byte: i8) -> bool {
+	signed_less_than(flag_byte) && zero(flag_byte)
+}
+//branching functions done
+
 enum Flags {
     Carry = 2,
    	Zero = 4,
@@ -15,6 +78,7 @@ struct Graphics {
 	
 struct StateRegister {
     bg: u8,
+	fg: u8,
 	spritew: u8,
 	spriteh: u8,
 	hflip: bool,
@@ -34,7 +98,17 @@ pub struct Cpu {
 
 impl StateRegister {
     pub fn new() -> StateRegister{
-	    StateRegister { bg: 0, spritew: 0, spriteh: 0, hflip: false, vflip: false,}
+	    StateRegister { bg: 0, fg: 0, spritew: 0, spriteh: 0, hflip: false, vflip: false,}
+	}
+	
+	fn clear(self: &mut StateRegister) {
+		self.fg = 0;
+		self.bg = 0;
+	}
+	
+	fn set_spr(self: &mut StateRegister, ll: i8, hh: i8) {
+		self.spritew = ll as u8;
+		self.spriteh = hh as u8;
 	}
 }
 
@@ -46,6 +120,14 @@ impl Graphics {
 		        0xEAD979, 0x537A3B, 0xABD54A, 0x252E38, 0x00467F, 0x68ABCC, 0xBCDEE4, 0xFFFFFF],
 		    screen: [[0, ..320], ..240],
 		}
+	}
+	
+	fn clear(self: &mut Graphics) {
+	    self.state.clear();
+	}
+	
+	fn spr(self: &mut Graphics, ll: i8, hh: i8) {
+		self.state.set_spr(ll, hh);
 	}
 }
 	
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod opcode;
 mod cpu;
 
 fn main() {
