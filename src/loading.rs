09f7a71f097c0da07fe4a42c4452556d91b1a93e
Tomas Coiro <tomcoiro@hotmail.com>
2014-12-17 16:12:39 -0300

    Added ROM size checking and fixed small error when loading C16 files

--- src/loading.rs
+++ src/loading.rs
@@ -20,7 +20,25 @@ pub fn load_c16(file: &mut File, cpu: &mut Cpu) -> () {
 		Err(e) => panic!("{}", e.desc)
 	};
 	if magic_number == 0x43483135 {
-		//Do verifications (checksum, size check)
+		file.read_u8();
+		file.read_u8();
+
+		let rom_size: u32 = match file.read_be_u32() {
+			Ok(rom) => rom,
+			Err(e) => panic!("{}", e.desc)
+		};
+		let checksum: u32 = match file.read_be_u32(){
+			Ok(sum) => sum,
+			Err(e) => panic!("{}", e.desc)
+		};
+		cpu.pc = match file.read_be_i16() {
+			Ok(ip) => ip,
+			Err(e) => panic!("{}", e.desc)
+		};
+
+		check_rom_size(file, rom_size);
+		crc32_checksum(file, checksum);
+
 		match file.seek(0x10, io::SeekSet){
 			Ok(ok) => ok,
 			Err(e) => panic!("{}", e.desc)
@@ -34,3 +52,18 @@ pub fn load_c16(file: &mut File, cpu: &mut Cpu) -> () {
 		load_bin(file, cpu);
 	}
 }
+
+fn check_rom_size(file: &mut File, rom_size: u32) -> () {
+	let file_size = match file.stat() {
+		Ok(file_stat) => file_stat.size - 0x10,
+		Err(e) => panic!("{}", e.desc)
+	};
+	if rom_size as u64 != file_size {
+		panic!("The ROM size is not what the header says");
+	}
+}
+
+fn crc32_checksum(file: &mut File, checksum: u32) -> () {
+	()
+}
+
